e848064aa8062964e4f7ef6c85b72af0b3765611
Laurent Peuch <cortex@worlddomination.be>
2014-01-13 05:03:08 +0100

    [enh] bugfix release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='icerockettail',
-      version='0.1',
+      version='0.1.1',
       description='equivalent of tail -f on icerocket twitter search results',
       author='Laurent Peuch',
       #long_description='',
